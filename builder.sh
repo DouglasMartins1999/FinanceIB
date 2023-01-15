8a5028da3d6a256aa4271465469c1f4a6ad0a885
Douglas Martins <douglas.martins.m099@gmail.com>
2023-01-15 11:40:57 -0300

    [Change] Builder Flags & Extension

--- builder.sh
+++ builder.sh
@@ -2,7 +2,7 @@
 GOOS=linux GOARCH=386 go build -o dist/financeIB_linux32
 GOOS=linux GOARCH=amd64 go build -o dist/financeIB_linux64
 GOOS=linux GOARCH=arm64 go build -o dist/financeIB_linuxARM
-GOOS=windows GOARCH=386 go build -o dist/financeIB_win32.exe
-GOOS=windows GOARCH=amd64 go build -o dist/financeIB_win64.exe
-GOOS=darwin GOARCH=arm64 go build -o dist/financeIB_macOS_silicon
-GOOS=darwin GOARCH=amd64 go build -o dist/financeIB_macOS_intel
+GOOS=windows GOARCH=386 go build -o dist/financeIB_win32.exe -ldflags -H=windowsgui
+GOOS=windows GOARCH=amd64 go build -o dist/financeIB_win64.exe -ldflags -H=windowsgui
+GOOS=darwin GOARCH=arm64 go build -o dist/financeIB_macOS_silicon.app
+GOOS=darwin GOARCH=amd64 go build -o dist/financeIB_macOS_intel.app
